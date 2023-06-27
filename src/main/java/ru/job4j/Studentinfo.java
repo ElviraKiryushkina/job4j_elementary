7932b8c51b7b8eefae3f3dd86017143b9cefb072
ElviraKiryushkina <kiryushkina-elvira@mail.ru>
2023-06-27 11:32:27 +0300

    4.1. Разбор первой программы. [#159033  [#159033]]

---
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Studentinfo {
+    public static void main(String[] args) {
+        System.out.println("Elvira Kiryushkina");
+        System.out.println("16.05.2001");
+    }
+}
