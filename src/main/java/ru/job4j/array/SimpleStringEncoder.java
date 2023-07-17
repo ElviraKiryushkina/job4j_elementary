bf3f3768e7c736f19355313f19d5203f38227036
Elvira <kiryushkina-elvira@mail.ru>
2023-07-17 14:39:49 +0300

    3. Тестовое задание. Блок "Базовый синтаксис" [#505049  [#505049]] (refactor)

--- src/main/java/ru/job4j/array/SimpleStringEncoder.java
+++ src/main/java/ru/job4j/array/SimpleStringEncoder.java
@@ -3,29 +3,27 @@ package ru.job4j.array;
 public class SimpleStringEncoder {
     public static String encode(String input) {
         String result = "";
-        String count = "";
         char symbol = input.charAt(0);
         int counter = 1;
+
         for (int i = 1; i < input.length(); i++) {
             if (input.charAt(i) == symbol) {
-                counter ++;
-                count = "" + counter;
+                counter = counter + 1;
             } else {
-                result = result + "" + symbol + count;
-                symbol = input.charAt(i);
+                if (counter > 1) {
+                    result = result + symbol + counter;
+                } else {
+                    result = result + symbol;
+                }
                 counter = 1;
-                count = "";
             }
-
-
+            if (i == input.length() - 1) {
+                symbol = input.charAt(i);
+                result = result + symbol;
+            }
+            symbol = input.charAt(i);
         }
-        result = result + symbol + count;
         return result;
     }
 
-
-    public static void main(String[] args) {
-        System.out.println(encode("zzzzhhhooopppppka"));
-
-    }
 }
